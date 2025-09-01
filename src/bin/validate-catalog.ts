b1c34f00ed9a262251d2d24b3e6c46fd17730405
Lars Willighagen <lars.willighagen@gmail.com>
2025-09-01 20:33:47 +0200

    fix(catalog): clearer error message when validating

--- src/bin/validate-catalog.ts
+++ src/bin/validate-catalog.ts
@@ -4,28 +4,28 @@ import { promises as fs } from 'fs'
 import * as path from 'path'
 import { catalog } from '../index'
 
-async function validateFile (arg: string): Promise<{ filePath: string, errors: WorkError[]}> {
+async function validateFile (arg: string): Promise<WorkError[]> {
     const filePath = path.resolve(arg)
     const file = await fs.readFile(filePath, 'utf8')
     const sheet = path.basename(filePath, '.csv')
-    return {
-        filePath,
-        errors: catalog.loadData(file, sheet).validate()
-    }
+    return catalog.loadData(file, sheet).validate()
 }
 
 async function main (args: string[]): Promise<void> {
     let exitStatus = 0
 
     const results = await Promise.allSettled(args.map(validateFile))
-    for (const result of results) {
+    for (let i = 0; i < results.length; i++) {
+        const result = results[i]
+
         if (result.status === 'rejected') {
+            console.error(`${args[i]}:`)
             console.error(result.reason)
             console.error()
             exitStatus = 1
-        } else if (result.value.errors.length > 0) {
-            console.error(`${result.value.filePath}:`)
-            console.table(result.value.errors)
+        } else if (result.value.length > 0) {
+            console.error(`${args[i]}:`)
+            console.table(result.value)
             console.error()
             exitStatus = 1
         }
